e2f3c85194ee546922b66fd1914db3fce726c3ba
SavindaJayasekara <thantrige32@gmail.com>
2023-10-16 17:27:14 +0530

    Re run

--- thogakade-web/App.py
+++ thogakade-web/App.py
@@ -76,15 +76,16 @@ def item_update():
 
 @app.route('/item_insert', methods=['POST'])
 def item_insert():
-    id = request.form['item-id']
-    description = request.form['item-name']
-    qty = int(request.form['item-qty'])
-    price = float(request.form['item-price'])
-    print("update")
-    cur.execute("INSERT INTO item ( item_id, item_name, item_quantity,  unit_price) VALUES (%s, %s, "
-                "%s, %s)", (id, description, qty, price))
-    con.commit()
-    return redirect(url_for('Index'))
+    if request.method == "POST":
+        id = request.form['itemID']
+        description = request.form['itemName']
+        qty = int(request.form['itemQty'])
+        price = float(request.form['itemPrice'])
+        print("update")
+        cur.execute("INSERT INTO item ( item_id, item_name, item_quantity,  unit_price) VALUES (%s, %s, "
+                    "%s, %s)", (id, description, qty, price))
+        con.commit()
+        return redirect(url_for('Index'))
 
 
 if __name__ == "__main__":
